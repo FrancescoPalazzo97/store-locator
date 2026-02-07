f998f56e7cb96f27685c226ca030639c849355d0
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2026-02-07 16:11:11 +0100

    feat: fix import path for HomePage and add HomePage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Header } from "./components/layout/Header";
 import { Footer } from "./components/layout/Footer";
-import { HomePage } from "./pages/HomPage";
+import { HomePage } from "./pages/HomePage";
 import { StoreDetailsPage } from "./pages/StoreDetailsPage";
 
 export const App = () => {
